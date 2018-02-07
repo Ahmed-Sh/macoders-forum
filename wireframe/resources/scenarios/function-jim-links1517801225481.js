b26627003ad46ba20dc8748458fcdd96dbfc8776
ahmedalrifai <ahmed.alrifai.97@gmail.com>
2018-02-07 16:38:55 +0200

    moving wireframe in a directory named wireframe/

---
+++ wireframe/resources/scenarios/function-jim-links1517801225481.js
@@ -0,0 +1,49 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "eb270a93-43d6-464d-8cd8-9ebce35a0234" : {
+      "Text_cell_3" : [
+        "9dcc54d6-2070-4bb9-a5a3-139884f0e64f"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Rectangle_1" : [
+        "f5ba6248-49b9-4cbc-8f8f-39474d506b8d"
+      ],
+      "Rectangle_2" : [
+        "f5ba6248-49b9-4cbc-8f8f-39474d506b8d"
+      ],
+      "Rectangle_3" : [
+        "f5ba6248-49b9-4cbc-8f8f-39474d506b8d"
+      ],
+      "Rectangle_4" : [
+        "f5ba6248-49b9-4cbc-8f8f-39474d506b8d"
+      ],
+      "Rectangle_5" : [
+        "f5ba6248-49b9-4cbc-8f8f-39474d506b8d"
+      ]
+    },
+    "d7df6e36-6b19-429b-ae85-d9e9c8958638" : {
+      "Label_30" : [
+        "e4341931-208b-4d2e-b953-915bb202134a"
+      ],
+      "Label_32" : [
+        "eb270a93-43d6-464d-8cd8-9ebce35a0234"
+      ],
+      "Text_3" : [
+        "1dfa48e9-225a-4db4-83d6-467a7fce701a"
+      ],
+      "Label_29" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Input_1" : [
+        "eb270a93-43d6-464d-8cd8-9ebce35a0234"
+      ],
+      "Image_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
