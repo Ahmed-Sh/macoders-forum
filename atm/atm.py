4154b30894513d8322092f120e564a3eacfa64d0
ahmedalrifai <ahmed.alrifai.97@gmail.com>
2018-02-09 01:45:21 +0200

    Edit atm.py the it have function withdraw()

--- atm/atm.py
+++ atm/atm.py
@@ -1,31 +1,47 @@
-def request_money(request, money):
-    if request <= money:
-        if request <= 0:
-            print "you can't request 0 or negative numbers"
-        else:
-            while request > 0:
-                if request >= 100:
-                    print "give " + str(100)
-                    request -= 100
-                elif request < 100 and request >= 50:
-                    print "give " + str(50)
-                    request -= 50
-                elif request < 50 and request >= 10:
-                    print "give " + str(10)
-                    request -= 10
-                elif request < 10 and request >= 5:
-                    print "give " + str(5)
-                    request -= 5
-                else:
-                    print "give " + str(request)
-                    request -= request
+def withdraw(balance, request):
+    # allowed papers: 100, 50, 10, 5, and cents
+
+    remaining_balance = balance - request
+
+    print("Current balance = %d" % balance)
+
+    if   request > balance:
+        print("Can't give you all this money !!")
+
+    elif request < 0:
+        print("More than zero plz!")
 
     else:
-        print "Dude, you don't have this amount of money!!!"
+        while request > 0:
+
+            if request >= 100:
+                request -= 100
+                print("give 100")
+
+            elif request >= 50:
+                request -= 50
+                print("give 50")
+
+            elif request >= 10:
+                request -= 10
+                print("give 10")
+
+            elif request >= 5:
+                request -= 5
+                print("give 5")
+
+            elif request < 5:
+                print("give " + str(request))
+                request = 0
+
+    return remaining_balance
 
 
 if __name__ == '__main__':
-    money = 500
-    request = 277
 
-    request_money(request, money)
+    balance = 500
+
+    balance = withdraw(balance, 277)
+    balance = withdraw(balance, 30)
+    balance = withdraw(balance, 5)
+    balance = withdraw(balance, 500)
