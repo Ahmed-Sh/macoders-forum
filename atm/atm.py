3c3f862343f5399230714369105599fc22cc47e9
ahmedalrifai <ahmed.alrifai.97@gmail.com>
2018-02-10 05:00:31 +0200

    Edit atm.py to have a class ATM for making more Than one ATM

--- atm/atm.py
+++ atm/atm.py
@@ -1,47 +1,62 @@
-def withdraw(balance, request):
-    # allowed papers: 100, 50, 10, 5, and cents
+class ATM:
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
 
-    remaining_balance = balance - request
+    def withdraw(self, request):
+        # allowed papers: 100, 50, 10, 5, and cents
 
-    print("Current balance = %d" % balance)
+        remaining_balance = self.balance - request
 
-    if   request > balance:
-        print("Can't give you all this money !!")
+        print("Welcome to " + self.bank_name)
+        print("Current balance = %d" % self.balance)
 
-    elif request < 0:
-        print("More than zero plz!")
+        print("=" * 15)
 
-    else:
-        while request > 0:
+        if request > self.balance:
+            print("Can't give you all this money !!")
 
-            if request >= 100:
-                request -= 100
-                print("give 100")
+        elif request < 0:
+            print("More than zero plz!")
 
-            elif request >= 50:
-                request -= 50
-                print("give 50")
+        else:
+            while request > 0:
 
-            elif request >= 10:
-                request -= 10
-                print("give 10")
+                if request >= 100:
+                    request -= 100
+                    print("give 100")
 
-            elif request >= 5:
-                request -= 5
-                print("give 5")
+                elif request >= 50:
+                    request -= 50
+                    print("give 50")
 
-            elif request < 5:
-                print("give " + str(request))
-                request = 0
+                elif request >= 10:
+                    request -= 10
+                    print("give 10")
 
-    return remaining_balance
+                elif request >= 5:
+                    request -= 5
+                    print("give 5")
+
+                elif request < 5:
+                    print("give " + str(request))
+                    request = 0
+
+        print("=" * 15)
+
+        self.balance = remaining_balance
 
 
 if __name__ == '__main__':
 
-    balance = 500
+    balance1 = 500
+    balance2 = 1000
 
-    balance = withdraw(balance, 277)
-    balance = withdraw(balance, 30)
-    balance = withdraw(balance, 5)
-    balance = withdraw(balance, 500)
+    atm1 = ATM(balance1, "Smart Bank")
+    atm2 = ATM(balance2, "Baraka Bank")
+
+    atm1.withdraw(277)
+    atm1.withdraw(800)
+
+    atm2.withdraw(100)
+    atm2.withdraw(2000)
