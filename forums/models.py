d0e771ed3fd6940aa1787b2b78c62a00e2bb8d76
ahmedalrifai <ahmed.alrifai.97@gmail.com>
2018-02-14 02:17:29 +0200

    Add string represintation for Member and Post

--- forums/models.py
+++ forums/models.py
@@ -3,8 +3,14 @@ class Member:
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return self.name
+
 
 class Post:
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
+    def __str__(self):
+        return self.title
