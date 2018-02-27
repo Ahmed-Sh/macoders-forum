2bb38a4538cc5ba04e9797b9ffef23bc717e2f55
ahmedalrifai <ahmed.alrifai.97@gmail.com>
2018-02-27 22:26:51 +0200

    Edits for 27 task

--- django_demo/django_demo/views.py
+++ django_demo/django_demo/views.py
@@ -1,10 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def hello(request):
+def home(request):
     return render(request, 'index.html')
-
-
-def say_hello(request, name=None):
-    return HttpResponse(f"Hello {name}")
