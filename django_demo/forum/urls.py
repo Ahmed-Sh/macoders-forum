7a882d807d3ec952634738a6e5ba95e94eedadf7
ahmedalrifai <ahmed.alrifai.97@gmail.com>
2018-03-02 03:50:19 +0200

    Update for task 31

--- django_demo/forum/urls.py
+++ django_demo/forum/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, AddPost
 
 urlpatterns = [
-    path('', home),
-    path('index/', home),
+    path('', home, name='home'),
+    path('post/add', AddPost.as_view(), name='add_post')
 ]
